b155dd134d6990fa69c2da15d4ef29d2d49011c2
andreaselenaa <159645343+andreaselenaa@users.noreply.github.com>
2026-02-26 21:56:31 -0400

    Update dashboard.py

--- App/controllers/dashboard.py
+++ App/controllers/dashboard.py
@@ -4,10 +4,10 @@ def get_admin_data():
     return Institution.query.all()
 
 def get_hr_stats(institution_id):
-    total_reg=Participant.query.filter_by(institution_id).count()
+    total_reg=Participant.query.filter_by(institution_id=institution_id).count()
     participated=db.session.query(Registration).join(Participant).filter(Participant.institution_id == institution_id).join(Result).distinct().count()
     participants=Participant.query.filter_by(institution_id=institution_id).all()
-    return{
+    return {
         "reg_count": total_reg,
         "part_count": participated,
         "no_show_count": total_reg - participated,
